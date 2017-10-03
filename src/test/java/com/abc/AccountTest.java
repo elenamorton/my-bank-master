eb15eb02415feae7a4a725e4abc90446b3110a77
emorton <elena.uleia@gmail.com>
2017-10-03 18:46:23 +0100

    Adding test case for deposit amount > 0.

--- src/test/java/com/abc/AccountTest.java
+++ src/test/java/com/abc/AccountTest.java
@@ -5,12 +5,21 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
+    private static final double DOUBLE_DELTA = 1e-15;
 
     @Test //Test account type
     public void accountType() {
     	Account savingsAccount = new Account(Account.SAVINGS);
 
         assertEquals(1, savingsAccount.getAccountType());
+    }
+
+    @Test //Test account deposit with amount > 0
+    public void accountDeposit() {
+    	Account savingsAccount = new Account(Account.SAVINGS);
+    	savingsAccount.deposit(2000.0);
+
+        assertEquals(2000, savingsAccount.sumTransactions(), DOUBLE_DELTA);
     }
 
 }
