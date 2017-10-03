d5cca6a2fec0caba51de3d7fa54a3c84ddb7838c
emorton <elena.uleia@gmail.com>
2017-10-03 20:47:01 +0100

    Adding test case comments, renamed three tests.

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class BankTest {
     private static final double DOUBLE_DELTA = 1e-15;
 
-    @Test
+    @Test //Test for first customer name
     public void getFirstCustomerExists() {
         Bank bank = new Bank();
         Customer john = new Customer("John");
@@ -19,14 +19,14 @@ public class BankTest {
         assertEquals("John", bank.getFirstCustomer());
     }
 
-    @Test
+    @Test //Test for first customer when none added to the bank
     public void getFirstCustomerNotExists() {
         Bank bank = new Bank();
 
         assertEquals("Error", bank.getFirstCustomer());
     }
 
-    @Test
+    @Test //Test for customer open accounts, when one account was opened
     public void customerSummarySingleAccount() {
         Bank bank = new Bank();
         Customer john = new Customer("John");
@@ -38,7 +38,7 @@ public class BankTest {
         assertEquals("Customer Summary\n - John (1 account)", bank.customerSummary());
     }
 
-    @Test
+    @Test //Test for customer open accounts, when two accounts were opened
     public void customerSummaryMultipleAccounts() {
         Bank bank = new Bank();
         Customer jane = new Customer("Jane");
@@ -52,8 +52,8 @@ public class BankTest {
         assertEquals("Customer Summary\n - Jane (2 accounts)", bank.customerSummary());
     }
 
-    @Test
-    public void checkingAccount() {
+    @Test //Test for interest paid on a checking account
+    public void checkingAccountInterest() {
         Bank bank = new Bank();
         Customer bill = new Customer("Bill");
         Account checkingAccount = new Account(Account.CHECKING);
@@ -65,8 +65,8 @@ public class BankTest {
         assertEquals(0.1, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
 
-    @Test
-    public void savingsAccount() {
+    @Test //Test for interest paid on a savings account
+    public void savingsAccountInterest() {
         Bank bank = new Bank();
         Customer bill = new Customer("Bill");
         Account savingsAccount = new Account(Account.SAVINGS);
@@ -78,8 +78,8 @@ public class BankTest {
         assertEquals(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
 
-    @Test
-    public void maxiSavingsAccount() {
+    @Test //Test for interest paid on a maxi-savings account
+    public void maxiSavingsAccountInterest() {
         Bank bank = new Bank();
         Customer bill = new Customer("Bill");
         Account maxiSavingsAccount = new Account(Account.MAXI_SAVINGS);
